13cf45fc3c0e87a957ab30a6395287be75d72c4a
kka <oceco@naver.com>
2024-12-05 10:54:46 +0900

    오픈그래프 이미지 추가

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,7 @@ useHead({
     { property: 'og:type', content: '식물도감' },
     { property: 'og:title', content: 'Gardeners Guide' },
     { property: 'og:url', content: 'https://house-plant-gardener.web.app' },
-    { property: 'og:image', content: 'https://house-plant-gardener.web.app/assets/opengragh.png' },
+    { property: 'og:image', content: 'https://house-plant-gardener.web.app/assets/opengragh-gMx8S_p-.png' },
     { property: 'og:description', content: '가드닝 정보를 제공하는 가이드북입니다.' },
   ],
 })
