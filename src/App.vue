17cd6e3a99e8b1cbea56a02774fd383a34cec81a
김근애 <gakim@proptier.co.kr>
2025-09-29 15:50:37 +0900

    useHead에서 파비콘 변경

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,27 @@
 </template>
 
 <script setup lang="ts">
-import {useHead} from '@unhead/vue'
+import { useHead } from '@unhead/vue'
 
 useHead({
   title: 'Gardeners Guide',
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ],
   meta: [
     { name: 'description', content: '가드닝 정보를 제공하는 가이드북입니다.' },
     { property: 'og:type', content: '식물도감' },
     { property: 'og:title', content: 'Gardeners Guide' },
     { property: 'og:url', content: 'https://house-plant-gardener.web.app' },
-    { property: 'og:image', content: 'https://house-plant-gardener.web.app/assets/opengragh-gMx8S_p-.png' },
+    {
+      property: 'og:image',
+      content: 'https://house-plant-gardener.web.app/assets/opengragh-gMx8S_p-.png'
+    },
     { property: 'og:description', content: '가드닝 정보를 제공하는 가이드북입니다.' }
-  ],
+  ]
 })
 </script>
